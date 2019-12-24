f41e8f82706ff4448c5f0150109260a4c0b7b2d6
afsori <afsoryafsor@gmail.com>
2019-12-24 13:43:41 +0700

    update appjs

--- App.js
+++ App.js
@@ -1,24 +1,22 @@
-
-import React, {Component} from 'react';
-import {View, Text} from 'react-native';
-import MainNav from './src/components/navigations/Main';
-
-class App extends Component {
-  render() {
-    return <MainNav />;
-  }
-
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 // import MainNav from './src/components/navigations/Main'
-// import Login from './src/components/screens/Login'
+
+// class App extends Component {
+//   render() {
+//     return <MainNav />;
+//   }
+
+// import { View, Text } from 'react-native'
+// // import MainNav from './src/components/navigations/Main'
+// // import Login from './src/components/screens/Login'
+// import ListDukCapil from './src/components/screens/ListDukCapil'
 import Main from './src/components/navigations/Main'
 
 class App extends Component {
 	render() {
 		return <Main />
 	}
-
 }
 
-export default App;
+export default App
